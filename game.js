265f26e18a814c62d057d2e8f90c4502f8c2455a
LemarTokham <ltokham@gmail.com>
2023-05-18 21:18:04 +0100

    Create game conditionals

--- game.js
+++ game.js
@@ -1,14 +1,26 @@
 console.log("Hello, World!");
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
-    console.log(choices[Math.floor(Math.random()* 3)])
-    return choices[Math.floor(Math.random()* 2)];
+    return choices[Math.floor(Math.random()* 3)];
 }
 
 function getPlayerChoice(playerSelection, computerSelection) {
     if (computerSelection === playerSelection) {
         console.log("Draw")
-    }
+    } else if (computerSelection == "Rock" && playerSelection == "Paper") {
+        console.log("You Win, Computer chose Rock")
+    } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
+        console.log("You Lose, Computer chose Rock");
+    } else if (computerSelection == "Paper" && playerSelection == "Scissors") {
+        console.log("You Win, Computer chose Paper");
+    } else if (computerSelection == "Paper" && playerSelection == "Rock") {
+        console.log("You Lose, Computer chose Paper");
+    } else if (computerSelection == "Scissors" && playerSelection == "Paper") {
+        console.log("You Lose, Computer chose Scissors");
+    } else if (computerSelection == "Scissors" && playerSelection == "Rock") {
+        console.log("You Win, Computer chose Scissors");
+    } 
 }
 
-getComputerChoice();
+let userChoice = prompt("Choice: ")
+getPlayerChoice(userChoice, getComputerChoice())
