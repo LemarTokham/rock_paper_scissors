7ca356db43de713d4ef02efc99997ff7ebf5ea34
LemarTokham <ltokham@gmail.com>
2023-05-18 21:10:33 +0100

    Add ComputerChoice function

--- game.js
+++ game.js
@@ -1,2 +1,14 @@
 console.log("Hello, World!");
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    console.log(choices[Math.floor(Math.random()* 3)])
+    return choices[Math.floor(Math.random()* 2)];
+}
 
+function getPlayerChoice(playerSelection, computerSelection) {
+    if (computerSelection === playerSelection) {
+        console.log("Draw")
+    }
+}
+
+getComputerChoice();
